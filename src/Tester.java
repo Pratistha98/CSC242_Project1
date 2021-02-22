2ef5ee03e0f01ff2743d5b6bbe67e74fe5e1c4b3
forrestwargo <forrestew13@gmail.com>
2021-02-22 17:54:48 -0500

    added testers

--- src/Tester.java
+++ src/Tester.java
@@ -27,12 +27,77 @@ public class Tester {
         State init_s = Game.generate_init_state(4);
         Player human = new Human();
         Player mini = new Minimax();
-        Game.printBoard(init_s.getBoard());
+        /*Game.printBoard(init_s.getBoard());
         Actions test_actions_neg_1 = new Actions(init_s);
         System.out.println(test_actions_neg_1.actions);
         init_s.activePlayer = 1;
         Actions test_actions_1 = new Actions(init_s);
         System.out.println(test_actions_1.actions);
      //Game.play_game(human,mini,init_s,false);
+        test_init_state_actions();*/
+        test_init_state_actions();
+        test_known_problem_states();
     }
-}
+    public static void test_init_state_actions(){
+        ArrayList<String> actions_neg_1 =new ArrayList<String>();
+        //adds elements to the arraylist
+        actions_neg_1.add("01");
+        actions_neg_1.add("10");
+        actions_neg_1.add("23");
+        actions_neg_1.add("32");
+
+        ArrayList<String> actions_1 =new ArrayList<String>();
+        //adds elements to the arraylist
+        actions_1.add("02");
+        actions_1.add("13");
+        actions_1.add("20");
+        actions_1.add("31");
+        State init_s = Game.generate_init_state(4);
+        //Game.printBoard(init_s.getBoard());
+        Actions test_actions_neg_1 = new Actions(init_s);
+        //System.out.println(test_actions_neg_1.actions);
+        init_s.activePlayer = 1;
+        Actions test_actions_1 = new Actions(init_s);
+        if(actions_neg_1.equals(test_actions_neg_1.getActions())){
+         System.out.println("test 1 PASSED");
+        }else{
+         System.out.println("test 1 FAILED");
+        }
+
+
+        if(actions_1.equals(test_actions_1.getActions())){
+         System.out.println("test 2 PASSED");
+        }else{
+         System.out.println("test 2 FAILED");
+        }
+        //System.out.println(test_actions_1.actions);
+    }
+    public static void test_known_problem_states(){
+        int[][] board = new int[4][4];
+        board[0][0] = 1;
+        board[0][1] = 1;
+        board[0][2] = 1;
+        board[1][0] = -1;
+        board[1][1] = 1;
+        board[1][2] = 1;
+        //board[1][3] = 1;
+        board[2][0] = -1;
+        board[2][1] = -1;
+        board[2][2] = 1;
+        board[3][0] = -1;
+        board[3][1] = -1;
+
+        //Game.printBoard(board);
+        State test_state = new State(board,1);
+        Actions test_actions = new Actions(test_state);
+        ArrayList<String> actions =new ArrayList<String>();
+        if(actions.equals(test_actions.getActions())){
+            System.out.println("test 3 PASSED");
+        }else{
+            System.out.println("test 3 FAILED");
+        }
+
+    }
+
+
+    }
